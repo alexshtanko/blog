1e8f7fd49ad597e426a7158ef1c03c19e7dc6d36
alex <alexshtanko@gmail.com>
2020-07-30 15:38:42 +0300

    Добавил Login, Registration

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,10 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Blog</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/style/bootstrap.min.css">
+
+        <link rel="stylesheet" href="/style/style.css">
+
+        
     </head>
 
     <body>
@@ -17,13 +20,33 @@
                     <div class="col-12">
                         <a href="/">Home</a> |
                         <a href="/articles">Article</a> |
-                        <a href="/contact">Contact</a>
+                        <a href="/contact">Contact</a> |
+                        @if (Route::has('login'))
+                                @auth
+                                    <a href="{{ url('/home') }}">Home</a>
+                                @else
+                                    <a href="{{ route('login') }}">Login</a> |
+
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}">Register</a>
+                                    @endif
+                                @endauth
+                            
+                        @endif
                     </div>    
                 </div>    
             </div>
         </header>
 
-        <main class="content">@yield('content')</main>
+        <main class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        @yield('content')
+                    </div>    
+                </div>    
+            </div>
+        </main>
 
         <footer class="footer">
             <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', 'SiteController@index');
 Route::get('/articles', 'SiteController@articles');
 
 Route::get('/contact', 'SiteController@contact');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
